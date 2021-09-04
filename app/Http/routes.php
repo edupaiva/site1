66d7358e21d2b235b770fcea4c927143f0d5b05e
edupaiva <pro.eduardo@hotmail.com>
2021-09-04 01:08:34 -0300

    First Version

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,7 @@
 //Route::get('/', function () {    return view('welcome');});
 
 
-Route::get('/', 'PagesController@index');
-Route::get('/produtos', 'PagesController@produtos');
-Route::get('/estrutura', 'PagesController@estrutura');
-Route::get('/contatos', 'PagesController@contatos');
+Route::get('/', 'PagesController@index')->name('pages.home');
+Route::get('/produtos', 'PagesController@produtos')->name('pages.product');
+Route::get('/estrutura', 'PagesController@estrutura')->name('pages.structure');
+Route::get('/contatos', 'PagesController@contatos')->name('pages.contact');
